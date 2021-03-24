93a1a5722ed3f9824ffb79feff2aeed10405d344
yashashree07 <47655034+yashashree07@users.noreply.github.com>
2021-03-24 18:54:24 +0530

    Update TestRunner.java

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -12,7 +12,7 @@ import io.cucumber.junit.CucumberOptions;
 		,monochrome=true
 		,dryRun=false	
 		,tags=  "@TC_18_02_IsraeliInwardReturnPaymentFlow"
-		,plugin = {"pretty", "html:target/cucumber-html-report",
+		,plugin = {"pretty", "html:target/cucumber-html-report","json:target/cucumber-reports/Cucumber.json","junit:target/cucumber-reports/Cucumber.xml",
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 
 		)
